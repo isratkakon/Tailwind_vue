ac1f428884814f891ba72e4157b649f181dfd5cd
isratkakon <isratkakon0@gmail.com>
2024-05-31 10:17:12 +0600

    Navbar-p-1

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,24 @@
 <template>
-    <div>
+    <header class="flex justify-between items-center bg-white shadow-md h-[80px] px-5 lg:px-24 relative z-10"> 
+        <div class="text-3xl font-extrabold text-blue-600 hover:text-blue-700 cursor-pointer">
+            <router-link to="/"> <i class="bi bi-code"></i> RigRex </router-link>
+        </div>
         
-    </div>
+        <nav class="flex gap-4 md:gap-10 px-6 md:static absolute bg-white shadow-md md:shadow-none md:w-auto my-7 md:my-0 left-0 w-full">
+            <router-link to="/" class="text-slate-500 hover:text-slate-700 text-xl font-medium"> Home </router-link>
+            <router-link to="/about" class="text-slate-500 hover:text-slate-700 text-xl font-medium"> About </router-link>
+            <router-link to="/services" class="text-slate-500 hover:text-slate-700 text-xl font-medium"> Services</router-link>
+        </nav>
+        <div class="text-4xl text-black cursor-pointer md:hidden">
+            <i class="bi bi-x"></i>
+        </div>
+    </header>    
 </template>
+
+<script setup>
+ 
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Services.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+       Service Page
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style  scoped>
+
+</style>
